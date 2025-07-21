4e142bdbe4b12a04f0bc58d526892aa9564b8651
SaleemBangash <92725872+SaleemBangash@users.noreply.github.com>
2025-07-21 11:02:47 +0400

    Fix: use base-href for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f59a88cd4893f8b2154f02558bff0b03",
+const RESOURCES = {"flutter_bootstrap.js": "64dc73fde34f172d951f5ca0142df8ec",
 "version.json": "fb8d4678888024cd5f83d8522bb3a722",
 "index.html": "8104e3379a9e7a2d63de1cdbb53912f4",
 "/": "8104e3379a9e7a2d63de1cdbb53912f4",
